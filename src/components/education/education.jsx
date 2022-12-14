7725a93698da7e4a2e291b045e8099709059eff9
balajisaikumar2000 <balajisaikumar3@gmail.com>
2022-12-14 18:22:01 +0530

    changed

--- src/components/education/education.jsx
+++ src/components/education/education.jsx
@@ -2,32 +2,32 @@ import { useState } from "react";
 import "./education.scss";
 import BootstrapCarousel from "./BootstrapCarousel";
 export default function Education() {
-  const [index, setIndex] = useState(0);
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
-  const data = [
-    {
-      id: 1,
-      name: "Sri Venkateswara University college of Engineering --Btech",
-      year: "2019-2023",
-      branch: "Electronics and Communication Engineering",
-      Aggregate: "81%",
-    },
-    {
-      id: 2,
-      name: "Narayana Junior College, Kadapa  —Intermediate",
-      year: "2017-2019",
-      branch: "Maths,Physics,Chemistry",
-      Aggregate: "96%",
-    },
-    {
-      id: 3,
-      name: "Sree Shanthikethan E.M High School— SSC",
-      year: "2015-2016",
-      Aggregate: "97%",
-    },
-  ];
+  // const [index, setIndex] = useState(0);
+  // const handleSelect = (selectedIndex, e) => {
+  //   setIndex(selectedIndex);
+  // };
+  // const data = [
+  //   {
+  //     id: 1,
+  //     name: "Sri Venkateswara University college of Engineering --Btech",
+  //     year: "2019-2023",
+  //     branch: "Electronics and Communication Engineering",
+  //     Aggregate: "81%",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Narayana Junior College, Kadapa  —Intermediate",
+  //     year: "2017-2019",
+  //     branch: "Maths,Physics,Chemistry",
+  //     Aggregate: "96%",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Sree Shanthikethan E.M High School— SSC",
+  //     year: "2015-2016",
+  //     Aggregate: "97%",
+  //   },
+  // ];
   return (
     <div className="education" id="education">
       <h1>Certifications</h1>
